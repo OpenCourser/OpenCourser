b2d0260080f3521b51f2b4005d848596f2ab1deb
Bart Stefanski <contact@bstefanski.com>
2022-12-31 00:13:20 +0100

    feat: setup saas ui

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,10 +1,16 @@
 import { UserProvider } from '@auth0/nextjs-auth0/client';
+import { SaasProvider } from '@saas-ui/react';
 import React from 'react';
+import { theme } from 'styles/theme';
 
-export default function App({ Component, pageProps }) {
+// Copied from next.js docs and the AppProps type doesn't work
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function MyApp({ Component, pageProps }: any) {
   return (
     <UserProvider>
-      <Component {...pageProps} />
+      <SaasProvider theme={theme}>
+        <Component {...pageProps} />;
+      </SaasProvider>
     </UserProvider>
   );
 }
