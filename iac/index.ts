db6b2fca3f029946411c0d89f7999492a7040035
Bart Stefanski <contact@bstefanski.com>
2023-01-19 22:00:23 +0100

    feat: add pulumi & setup frontend infra on cloudflare

---
+++ iac/index.ts
@@ -0,0 +1,42 @@
+import * as pulumi from '@pulumi/pulumi';
+import * as cloudflare from '@pulumi/cloudflare';
+
+const cfg = new pulumi.Config();
+
+const basicProject = new cloudflare.PagesProject('openCourserWeb', {
+  accountId: cfg.require('cloudflare:accountId'),
+  name: 'opencourser-web',
+  productionBranch: 'main',
+  buildConfig: {
+    buildCommand: 'make build-web',
+    destinationDir: 'apps/web/.next',
+    rootDir: 'apps/web',
+  },
+  source: {
+    config: {
+      deploymentsEnabled: true,
+      owner: 'OpenCourser',
+      prCommentsEnabled: true,
+      previewBranchExcludes: ['main', 'prod'],
+      previewBranchIncludes: ['dev', 'preview'],
+      previewDeploymentSetting: 'custom',
+      productionBranch: 'main',
+      productionDeploymentEnabled: true,
+      repoName: 'OpenCourser',
+    },
+    type: 'github',
+  },
+  deploymentConfigs: {
+    preview: {
+      environmentVariables: {
+        ENVIRONMENT: 'preview',
+      },
+    },
+    production: {
+      environmentVariables: {
+        ENVIRONMENT: 'production',
+        OTHERVALUE: 'other value',
+      },
+    },
+  },
+});
