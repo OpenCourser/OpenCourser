6b50f68183fd8fca07b38c0a2130fc46bf7ca93f
Bart Stefanski <contact@bstefanski.com>
2023-02-15 19:49:03 +0100

    chore: bump all web dependencies

--- apps/web/styles/theme.ts
+++ apps/web/styles/theme.ts
@@ -1,6 +1,7 @@
 import { extendTheme } from '@chakra-ui/react';
 import { theme as baseTheme } from '@saas-ui/react';
+import { theme as glassTheme } from '@saas-ui/theme-glass';
 
 const colors = {};
 
-export const theme = extendTheme({ colors }, baseTheme);
+export const theme = extendTheme({ colors }, glassTheme, baseTheme);
